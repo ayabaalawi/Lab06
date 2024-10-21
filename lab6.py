3bcb6b1babf7c295c040a61221b115a6be73d1de
ayabaalawi <aya.baalawi@ufl.edu>
2024-10-21 19:05:00 +0400

    added encoder and main function

---
+++ lab6.py
@@ -0,0 +1,35 @@
+#name: aya ba alawi
+
+def encoder(password):
+    res = ''
+    for digit in str(password):
+        encoded_pass = int(digit) + 3
+        if len(str(encoded_pass)) >= 2:
+            encoded_pass =  encoded_pass - 10
+        res += str(encoded_pass) #concentating the string
+    return res
+
+def main():
+    program_running = True
+    while program_running:
+        print('\nMenu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+
+        menu_option = int(input('\nPlease enter an option: '))
+        if menu_option == 1:
+            user_password = input('Please enter your password to encode: ')
+            encoded_password = encoder(user_password)
+            print('Your password has been encoded and stored!')
+
+        elif menu_option == 2:
+            print(f'The encoded password is {encoded_password}, and the original password is {user_password}.')
+
+        elif menu_option == 3:
+            program_running = False
+
+
+if __name__ == '__main__':
+    main()
