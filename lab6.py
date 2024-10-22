b1b8a565f53eb14a1419755a45185aba7a1914cb
lcarvalhomaga <lcarvalhomagalha@ufl.edu>
2024-10-22 16:24:13 -0500

    Added decoder function

--- lab6.py
+++ lab6.py
@@ -9,6 +9,13 @@ def encoder(password):
         res += str(encoded_pass) #concentating the string
     return res
 
+# Lucca Carvalho Magalhaes
+def decoder(password):
+    decoded = ""
+    for i in password:
+        decoded += str((int(i) - 3) % 10) 
+    return decoded
+
 def main():
     program_running = True
     while program_running:
@@ -21,13 +28,11 @@ def main():
         menu_option = int(input('\nPlease enter an option: '))
         if menu_option == 1:
             user_password = input('Please enter your password to encode: ')
-            #when decode function is made, can replace above line with user_password = encode(user_password)
-            encoded_password = encoder(user_password)
+            user_password = encoder(user_password)
             print('Your password has been encoded and stored!')
 
         elif menu_option == 2:
-            #when decode function is made, can assign encoded_password to decode function result
-            print(f'The encoded password is {encoded_password}, and the original password is {user_password}.')
+            print(f'The encoded password is {user_password}, and the original password is {decoder(user_password)}.')
 
         elif menu_option == 3:
             program_running = False
